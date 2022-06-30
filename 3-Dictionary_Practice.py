644db3a381614dc67db44bedf8efb7bc1b7e0537
whabib <waqarhabib94@gmail.com>
2022-06-30 11:12:11 -0500

    practice updated

--- 3-Dictionary_Practice.py
+++ 3-Dictionary_Practice.py
@@ -17,3 +17,21 @@ myDictionary["office"] = "computer"
 myDictionary['kitchen'] = ['oven', 'microwave', 'fridge']
 
 #print(myDictionary)
+
+#access single item using index
+#create var and set equal to what you want to pull
+indexItem = myDictionary['kitchen'][2]
+#print(indexItem)
+
+#values in dict don't have to be same type. Can be strings, integers or both
+
+#below: adding items to dictionary with integers
+myDictionary = {3:"itemsInKitchen", 1:"itemsInOffice"}
+
+#prints entire dictionary with integer items
+#print(myDictionary)
+
+#print single item using the key of one of the items' values
+items = myDictionary[3]
+#print(items)
+
