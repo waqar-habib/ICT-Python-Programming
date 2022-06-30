bd7c14b423aed55151e9ea4bffd306bba2c47de9
whabib <waqarhabib94@gmail.com>
2022-06-30 10:09:19 -0500

    dictionary practice

---
+++ 3-Dictionary_Practice.py
@@ -0,0 +1,19 @@
+myDictionary = {"white" : "carpet",
+                "brown" : "chairs",
+                "gray" : "bed"
+                }
+
+#access item in dictionary: print(dictName["key"])
+#print(myDictionary["white"])
+
+#add items to dictionary
+myDictionary["green"] = "yeti"
+
+# adding more than one item to dictionary
+#1. individually
+myDictionary["bedroom"] = "dresser"
+myDictionary["office"] = "computer"
+#2. Multiple
+myDictionary['kitchen'] = ['oven', 'microwave', 'fridge']
+
+#print(myDictionary)
