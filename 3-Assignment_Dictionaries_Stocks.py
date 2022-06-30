751cd86d57115d85d646b3bb339d11ea9c42b3b4
whabib <waqarhabib94@gmail.com>
2022-06-30 11:06:12 -0500

    assignment 3 part 1

--- 3-Assignment_Dictionaries_Stocks.py
+++ 3-Assignment_Dictionaries_Stocks.py
@@ -4,3 +4,25 @@
 #Thu, Jun 30, 2022
 #Assignment 3
 
+import pprint as prettyPrint
+
+print("Part One - Dictionary")
+#converting previous lists to dictionary
+
+myStocks = {"META" : [10,325.20,163.74],
+            "AAPL" : [11,319.91,131.56],
+            "MSFT" : [15, 169.75,247.65],
+            "TSLA" : [20,899.94,650.28]
+            }
+# pretty printing dictionary
+prettyPrint.pprint(myStocks)
+
+# gaps
+print("-" * 30)
+print("\n" * 1)   
+print("Part Two - Investor Report")
+print("-" * 30)
+
+
+
+
