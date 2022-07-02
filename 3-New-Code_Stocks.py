dc63709911c0b00b31de27565c623bcdc119b8aa
whabib <waqarhabib94@gmail.com>
2022-07-02 15:49:56 -0500

    Fixed it after f'ing it up AGAIN

--- 3-New-Code_Stocks.py
+++ 3-New-Code_Stocks.py
@@ -5,39 +5,26 @@
 #Assignment 3
  
 
-#ln:10-17 - Looping through each stock from portfolio   
-
+#ln:9-14 - Looping through each stock from portfolio   
 myStocks = {
     'META': [10, 325.20, 163.74 ],
     'AAPL': [11, 319.91, 131.56 ],
     'MSFT': [15, 169.75, 247.65 ],
     'TSLA': [20, 899.94, 650.28 ],   
 }
-
-for symbol, listOfData in myStocks.items():
-    print(f"\n{symbol}")
-    for data in listOfData:
-        print(f"{data}")
         
- #ln:20-25 - Gain/Loss calculation for each stock from portfolio          
+#ln:16-25 - Gain/Loss calculation for each stock from portfolio          
 for symbol in myStocks:
      numShares = myStocks[symbol][0]
      pricePurchase = myStocks[symbol][1]
      priceNow = myStocks[symbol][2]
      gainLoss = numShares*priceNow - numShares*pricePurchase
-    
-#ln:27-34 - Printing stock name/shares and total gain or loss with formatting  
-for symbol in myStocks:
-    numShares = myStocks[symbol][0]
-    pricePurchase = myStocks[symbol][1]
-    priceNow = myStocks[symbol][2]
-    print(f"\nStock Name: {symbol}")
-    print(f"Shares: {numShares}")
-    currency = "${:,.2f}".format(gainLoss)
-    gainLoss = numShares*priceNow -      numShares*pricePurchase
-    print(f"Gain or Loss: {currency}")
-    
-# Conditional to print the stock with highest gain/loss
+     print(f"\nStock Name: {symbol}")
+     print(f"Shares: {numShares}")
+     currency = "${:,.2f}".format(gainLoss)
+     print(f"Gain or Loss: {currency}")
+  
+#ln:28-32 - Conditional to print the stock with highest gain/loss
 if gainLoss >= 0:
     currency = "${:,.2f}".format(gainLoss)
     print(f"\nThe highest earning stock is {symbol} with a total gain of: {currency}")
