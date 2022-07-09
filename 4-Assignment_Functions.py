3ab120d7c067c9409c215ab7d1520a39a88a68bf
whabib <waqarhabib94@gmail.com>
2022-07-09 16:27:36 -0500

    working code: prints table, no repeating list

--- 4-Assignment_Functions.py
+++ 4-Assignment_Functions.py
@@ -1,75 +1,25 @@
-
-from datetime import date
-from datetime import timedelta
-
-# Initiating lists
+# WORKING CODE: 07/09 @ 4:26; prints table perfectly
 symbols = ["A", "B", "C", "D"]
 numberShares = [10,11,15,20]
 pricePurchase = [40, 10, 20, 30]
 priceNow = [30,25,40,15]
-totalNow = []
-totalPurchase = []
-gainLoss = []
 
-# prints header
-print(f'-'*79 + f'|')
-print(f"|"+ '-' * 3 + 'Stock Name' + "-" *5 + 'Shares' + "-" *6 + 'Price Purchase' + "-" *5 + 'Price Now'+ "-" *5 + 'Gain/Loss'+'-'*5, f'|' )
-print(f'-'*79 + f'|')
-
-# Creating a function to print lists - passing positional arguments
-def printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss):
+# Creating a function to print lists
+def calculateGainLoss(symbols, numberShares, pricePurchase, priceNow):
+    # Header
+    print(f'-'*65 + f'|')
+    print(f"|"+ '-' * 3 + 'Stock Name' + "-" *5 + 'Shares' + "-" *6 + 'Price Purchase' + "-" *5 + 'Price Now'+ "-" *5, f'|' )
+    print(f'-'*65 + f'|')
     
     #for loop to loop through all stocks
     for index, oneStock in enumerate(symbols):
-        
-        #Calulcating Gain/Loss
-        for value1, value2 in zip(priceNow,numberShares):
-            totalNow.append(value1*value2)
-
-        for value1, value2 in zip(pricePurchase,numberShares):
-            totalPurchase.append(value1*value2)
-    
-        for value1, value2 in zip(totalNow,totalPurchase):
-            gainLoss.append(value1-value2)
-        
-        # prints each stock in separate lines
+        currencyPP = "${:,.2f}".format(pricePurchase[index])
+        currencyPN = "${:,.2f}".format(priceNow[index])
         print(f"|" + f"-"*7 + (f"{oneStock}") +'-' * 
-            (8-len(oneStock)) + f"-"*4 + f'|{numberShares[index]}'+'-'*10 
-            + '${:,.2f}'.format(pricePurchase[index]) 
-            +'-'*11 
-            + '${:,.2f}'.format(priceNow[index])
-            +'-'*8 
-            + '${:,.2f}'.format(gainLoss[index])
-            +'-'*8 
-            + f'|') 
+            (8-len(oneStock)) + f"-"*4 + f'|{numberShares[index]}'+'-'*10 + f'|{currencyPP}'+'-'*11 + f'|{currencyPN}'+'-'*7+ f'|') 
+        print(f'-'*65 + f'|')
         
-        #prints separator
-        print(f'-'*79 + f'|')
-        
-        # def calcYoY(gainLoss,pricePurchase):
-        #     currentDate = date.today()
-        #     purchaseDate = currentDate - timedelta(days = 1801)
-            
-        #     print(
-        #         ((gainLoss[index]/pricePurchase[index]))
-        #     )
-        
-        # calcYoY(gainLoss,priceNow)
-        
-        
+# Initializing the calculateGainLoss function
+calculateGainLoss(symbols, numberShares, pricePurchase, priceNow)
 
-# Initializing the calculateGainLoss function - passing parameters
-printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss)
-
-print('*'*100)
-print("YoY Calculation")
-print('*'*100)
-
-# Get today's date
-currentDate = date.today()
-print("Today is: ", currentDate)
- 
-# Yesterday date
-purchaseDate = currentDate - timedelta(days = 1801)
-print("Purchase Date was: ", purchaseDate)
 
