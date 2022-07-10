cca550bd80fe4d89deef62de5805a1d443b9123e
whabib <waqarhabib94@gmail.com>
2022-07-09 23:59:24 -0500

    working code: added line comments

--- 4-Assignment_Functions.py
+++ 4-Assignment_Functions.py
@@ -11,12 +11,12 @@ totalNow = []
 totalPurchase = []
 gainLoss = []
 
-# prints header
+# (ln 15-17) prints table header
 print(f'-'*100 + f'|')
 print(f"|"+ '-' * 3 + 'Stock Name' + "-" *5 + 'Shares' + "-" *6 + 'Price Purchase' + "-" *5 + 'Price Now'+ "-" *5 + 'Gain/Loss'+'-'*9 + 'YoY'+'-'*13, f'|' )
 print(f'-'*100 + f'|')
 
-# Second function to calculate Gain or Loss
+# (ln 20-27/needs refactoring) Second function to calculate Gain or Loss
 def calculateGainLoss(priceNow, pricePurchase, numberShares, totalNow, totalPurchase):
     #Calulcating Gain/Loss
     for value1, value2 in zip(priceNow,numberShares):
@@ -26,7 +26,7 @@ def calculateGainLoss(priceNow, pricePurchase, numberShares, totalNow, totalPurc
     for value1, value2 in zip(totalNow,totalPurchase):
         gainLoss.append(value1-value2)
         
-# First function: Moved code from last week in a separate function - passing positional arguments
+# (ln 30-59) First function: Moved code from last week in a separate function - passing positional arguments
 def printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss):
     
     #for loop to loop through all stocks
@@ -35,11 +35,12 @@ def printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss):
         # Calling calculateGainLoss function from inside the for loop
         calculateGainLoss(priceNow, pricePurchase, numberShares, totalNow, totalPurchase)
         
+        # Calculating daysSincePurchase to plug into ln 52
         todaysDate = date.today()
         purchaseDate = date(2017, 8, 1)
         daysSincePurchase = ((todaysDate-purchaseDate).days)/365
     
-        # prints each stock in separate lines
+        # (ln44-54)) prints each stock in separate rows in table
         print(f"|" + f"-"*7 + (f"{oneStock}") +'-' * 
                 (8-len(oneStock)) + f"-"*4 + f'|{numberShares[index]}'+'-'*10 
                 + '${:,.2f}'.format(pricePurchase[index]) 
@@ -52,7 +53,7 @@ def printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss):
                 +'-'*5
                 + f'|') 
         
-        #prints separator
+        # prints separator
         print(f'-'*93 + f'|')
         
 # Initializing the calculateGainLoss function - passing parameters
@@ -60,10 +61,14 @@ printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss)
 
 # Third function to calculate year over year yield    
 def calculateYoY (symbols, gainLoss, pricePurchase):
+       
+        # (ln 65-67) Had to repeat daysSincePurchase code because couldn't figure out how to call it in printTable function in line 38-41 :(
+            
         todaysDate = date.today()
         purchaseDate = date(2017, 8, 1)
         daysSincePurchase = ((todaysDate-purchaseDate).days)/365
         
+        # (ln 72-77) Loop through stocks & calculate % yield  
         for j in range(0, len(symbols)):
             YoY = ((gainLoss[j]/pricePurchase[j])/daysSincePurchase)*100
             if YoY >= 0:
@@ -72,4 +77,15 @@ def calculateYoY (symbols, gainLoss, pricePurchase):
                 print(f"You lost {'{:,.2f}'.format(YoY)}% on your {symbols[j]} stock")
     
 calculateYoY(symbols, gainLoss, pricePurchase)
+
+
+'''
+Note to self: 
+You created a new function calculateYoY (63-79) to print 8 statements based on if you gained/lost x% on each stock. 
+to Do:
+1. Fix formatting
+2. move funtions to modules
+3. refactor 2nd function calculateGainLoss
+
+'''
 
