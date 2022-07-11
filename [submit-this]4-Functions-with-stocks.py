6e8b392430583b16c055f2321e3cdc362d47a315
whabib <waqarhabib94@gmail.com>
2022-07-10 21:55:25 -0500

    working code: moved calculateYoY to new file

--- [submit-this]4-Functions-with-stocks.py
+++ [submit-this]4-Functions-with-stocks.py
@@ -2,6 +2,7 @@
 
 from datetime import date
 from calculateGainLoss import calculateGainLoss
+from calculateYoY import calculateYoY
 
 # Initiating lists
 symbols = ["GOOGLE", "MSFT", "RDS-A", "AIG","FB","M","F","IBM"]
@@ -53,20 +54,7 @@ def printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss):
 # Initializing the calculateGainLoss function - passing parameters
 printTable(symbols, numberShares, pricePurchase, priceNow, gainLoss)
 
-# Third function to calculate year over year yield    
-def calculateYoY (symbols, gainLoss, pricePurchase):
-            
-        todaysDate = date.today()
-        purchaseDate = date(2017, 8, 1)
-        daysSincePurchase = ((todaysDate-purchaseDate).days)/365
-        
-        # (ln 72-77) Loop through stocks & calculate % yield  
-        for j in range(0, len(symbols)):
-            YoY = ((gainLoss[j]/pricePurchase[j])/daysSincePurchase)*100
-            if YoY >= 0:
-                print(f"You gained {'{:,.2f}'.format(YoY)}% on your {symbols[j]} stock")
-            else:
-                print(f"You lost {'{:,.2f}'.format(YoY)}% on your {symbols[j]} stock")
+
     
 calculateYoY(symbols, gainLoss, pricePurchase)
 
