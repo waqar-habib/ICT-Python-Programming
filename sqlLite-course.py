3bd530d42ba6fd4f6470e6171bf22a84619e225e
whabib <waqarhabib94@gmail.com>
2022-07-27 14:13:09 -0500

    working code

--- sqlLite-course.py
+++ sqlLite-course.py
@@ -1,1 +1,44 @@
 import sqlite3
+
+#connects or creates a db
+conn = sqlite3.connect('customer.db')
+
+#create a cursor (c)
+c = conn.cursor()
+
+
+
+
+#create a table
+# DATATYPES = null, real (10.5), integers (10), text (string), blob(images etc..)
+
+# c.execute("""CREATE TABLE customers (
+#     first_name text,
+#     last_name text,
+#     email text    
+# )""")
+
+# insert first entry
+c.execute("INSERT INTO customers (first_name, last_name, email) VALUES ('John', 'Elder', 'john@codemy.com')""")
+
+#insert many entries
+many_customers = [
+    ('a','b','a.b@t.com'),
+    ('w','h','w.h@t.com'),
+    ('m','k','m.k@t.com'),
+    ]
+
+c.executemany("INSERT INTO customers VALUES (?,?,?)",many_customers)
+
+
+# query db
+c.execute("SELECT * FROM customers")
+# c.fetchone()
+# c.fetchmany(3)
+print(c.fetchall())
+
+#save to DB
+conn.commit()
+
+#close connection
+conn.close()
